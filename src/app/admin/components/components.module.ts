309fbeccd59c084d5218409be0e2f9855e1e5e87
Ibrahim ALTSOY <i.altsoy@gmail.com>
2023-12-26 23:00:06 +0300

    Değişiklikleri commit et

--- src/app/admin/components/components.module.ts
+++ src/app/admin/components/components.module.ts
@@ -3,14 +3,14 @@ import { CommonModule } from '@angular/common';
 import { UserModule } from './user/user.module';
 import { DashboardModule } from './dashboard/dashboard.module';
 import { CustomerModule } from './customer/customer.module';
-
+import {MatSidenavModule} from '@angular/material/sidenav';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,UserModule,DashboardModule,
-    CustomerModule
+    CustomerModule,MatSidenavModule
   ]
 })
 export class ComponentsModule { }
